e094212392d719a112e39b5307aa9f38e34f0cdf
Carmen Araya Rodríguez <caarayar@uc.cl>
2022-12-26 11:44:43 -0300

    Página con orden año de estreno y alfabético en selectores separados

--- src/data.js
+++ src/data.js
@@ -1,9 +1,84 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+//función de orden por año de estreno
+export const sortDataRelease = (data, condition) => {
+  let dataOrdenadaA = "";
+  if (condition === "firstMovies") {
+    dataOrdenadaA = data.sort((a, b) => {
+      const firstParameter = a.release_date;
+      const secondParameter = b.release_date;
+      return (firstParameter < secondParameter) ? -1 : (firstParameter > secondParameter) ? 1 : 0;
+    })
+  } else {
+    dataOrdenadaA = data.sort((a, b) => {
+      const firstParameter = a.release_date;
+      const secondParameter = b.release_date;
+      return (firstParameter > secondParameter) ? -1 : (firstParameter < secondParameter) ? 1 : 0;
+    })
+  } return dataOrdenadaA;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+//función de orden alfabético
+export const sortDataAlph = (data, condition) => {
+  let dataOrdenadaB = "";
+  if (condition === "az") {
+    dataOrdenadaB = data.sort((a, b) => {
+      const firstParameter = a.title.toLowerCase();
+      const secondParameter = b.title.toLowerCase();
+      return (firstParameter < secondParameter) ? -1 : (firstParameter > secondParameter) ? 1 : 0;
+    })
+  } else {
+    dataOrdenadaB = data.sort((a, b) => {
+      const firstParameter = a.title.toLowerCase();
+      const secondParameter = b.title.toLowerCase();
+      return (firstParameter > secondParameter) ? -1 : (firstParameter < secondParameter) ? 1 : 0;
+    })
+  } return dataOrdenadaB;
 };
+
+// export const sortData = (sortData, ghibliAnimations) => {
+//     if (sortData === "firstMovies") {
+//       ghibliAnimations.sort((a, b) => {
+//         let firstParameter = a.release_date;
+//         let secondParameter = b.release_date;
+//         return (firstParameter < secondParameter) ? -1 : (firstParameter > secondParameter) ? 1 : 0;
+//       })
+//     }
+//     if (sortData === "lastMovies") {
+//       ghibliAnimations.sort((a, b) => {
+//         let firstParameter = a.release_date;
+//         let secondParameter = b.release_date;
+//         return (firstParameter > secondParameter) ? -1 : (firstParameter < secondParameter) ? 1 : 0;
+//       })
+//     }
+//     return ghibliAnimations;
+//   };
+
+//   //se ve la página pero no corre
+//    export const sortData = (sortData, ghibliAnimations) => {
+//        if (sortData === "firstMovies") {
+//          ghibliAnimations.sort((a, b) => {
+//            if (a.release_date < b.release_date) {
+//              return 1;
+//            } if (a.release_date > b.release_date) {
+//              return -1;
+//            } if (a.title.toLowerCase() < b.title.toLowerCase()) {
+//              return 1;
+//            } if (a.title.toLowerCase() > b.title.toLowerCase()) {
+//              return -1;
+//            }
+//            return 0;
+//          })
+//        } else if (sortData === "lastMovies") {
+//          ghibliAnimations.sort((a, b) => {
+//            if (a.release_date > b.release_date) {
+//              return 1;
+//            } if (a.release_date < b.release_date) {
+//              return -1;
+//            } if (a.title.toLowerCase() > b.title.toLowerCase()) {
+//              return 1;
+//            } if (a.title.toLowerCase() < b.title.toLowerCase()) {
+//              return -1;
+//            }
+//            return 0;
+//          })
+//        }
+//      };
--- src/main.js
+++ src/main.js
@@ -1,70 +1,50 @@
-import { example } from './data.js';
+import { sortDataAlph, sortDataRelease } from './data.js'; //impotando funciones
+import data from './data/ghibli/ghibli.js'; //importando data desde ghibli.js
 
-import data from './data/ghibli/ghibli.js';
+//llamando a la data desde ghibli.js
+const ghibliAnimations = data.films; 
 
-console.log(example, data);
-
-document.getElementById("button-up").addEventListener("click", scrollUp);
-
-function scrollUp(){
-  const currentScroll = document.documentElement.scrollTop;
-  if (currentScroll > 0){
-    window.scrollTo (0,0);
-  }
-}
-const movie = data.films
-// console.log(movie);
-
-// const movieOne = movie[0];
-// console.log(movieOne);
-
-// const tittle = movieOne.title;
-// console.log(tittle);
-
-// const director1 = data.films[0].director
-// console.log(director1);
-
-// const director5 = data.films[4].director
-// console.log(director5);
-
-// const poster = data.films[0].poster;
-// const title = data.films[0].title;
-// const director = data.films[0].director; 
-// const producer = data.films[0].producer; 
-// const year = data.films[0].release_date; 
-
-// console.log(poster, title, director, producer, year);
-
-//ejecutar una función por cada elemento que tenga mi arreglo
-// movie.forEach((element, i) => {
-//    console.log(`
-//    Title: ${data.films[i].title}
-//    Poster: ${data.films[i].poster}
-//    `)
-// });
-
-// movie.forEach((element, i) => {
-//     console.log(`
-//     <img src=${data.films[i].poster}>
-//     <h1> Title: ${data.films[i].title}</h1>
-//     <p>Director: ${data.films[i].director}</p>
-//     <p>Producer: ${data.films[i].producer}</p>
-//     <p>Year: ${data.films[i].release_date}</p>
-//     `);
-//  });
-
-
-const ghibliMovies = document.getElementById("movies");
-
-let movies = "";
-movie.forEach((element, i) => {
-  movies+=
-    `<div class="caja">
+//función para mostrar las tarjetas de películas
+function showAnimations() {
+  let movies = "";
+  ghibliAnimations.forEach((elements, i) => {
+    movies +=
+      `<div class="caja">
       <img class="poster" src=${data.films[i].poster} />
       <h3>${data.films[i].title}</h3>
       <p>Director: ${data.films[i].director}</p>
       <p>Producer: ${data.films[i].producer}</p>
       <p>Release date: ${data.films[i].release_date}</p>
       </div>`;
-});
-ghibliMovies.innerHTML = movies;
+  });
+  document.getElementById("animations").innerHTML = movies;
+}
+showAnimations(ghibliAnimations);
+
+//función para mostrar el orden
+document.getElementById("orderRelease").addEventListener("change", showOrderRelease);
+document.getElementById("orderAlph").addEventListener("change", showOrderAlph);
+
+//orden por año de estreno
+function showOrderRelease() {
+  const orderOption = document.getElementById("orderRelease").value;
+  const orderResult = sortDataRelease(ghibliAnimations, orderOption);
+  showAnimations(orderResult);
+}
+
+//orden alfabético
+function showOrderAlph() {
+  const orderOption = document.getElementById("orderAlph").value;
+  const orderResult = sortDataAlph(ghibliAnimations, orderOption);
+  showAnimations(orderResult);
+}
+
+//botón totoro
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp() {
+  const currentScroll = document.documentElement.scrollTop;
+  if (currentScroll > 0) {
+    window.scrollTo(0, 0);
+  }
+}
