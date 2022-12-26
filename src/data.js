f9ffcaa89cbb64308a5a626c1c201e2a5e76610c
Alejandra Castillo <castilloavilaa@gmail.com>
2022-12-26 11:37:55 -0500

    Unión de las opciones de orden en un selector

--- src/data.js
+++ src/data.js
@@ -1,84 +1,43 @@
-//función de orden por año de estreno
-export const sortDataRelease = (data, condition) => {
-  let dataOrdenadaA = "";
+//función para ordenar datos
+export const sortData = (data, condition) => {
+  let dataOrdenada = "";
   if (condition === "firstMovies") {
-    dataOrdenadaA = data.sort((a, b) => {
-      const firstParameter = a.release_date;
-      const secondParameter = b.release_date;
-      return (firstParameter < secondParameter) ? -1 : (firstParameter > secondParameter) ? 1 : 0;
+    dataOrdenada = data.sort((a, b) => {
+      if (a.release_date < b.release_date) {
+        return -1;
+      } if (a.release_date > b.release_date) {
+        return 1;
+      } if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1;
+      } if (a.title.toLowerCase() > b.title.toLowerCase()) {
+        return 1;
+      }
+      return 0;
     })
-  } else {
-    dataOrdenadaA = data.sort((a, b) => {
-      const firstParameter = a.release_date;
-      const secondParameter = b.release_date;
-      return (firstParameter > secondParameter) ? -1 : (firstParameter < secondParameter) ? 1 : 0;
+  } else if (condition === "lastMovies") {
+    dataOrdenada = data.sort((a, b) => {
+      if (a.release_date > b.release_date) {
+        return -1;
+      } if (a.release_date < b.release_date) {
+        return 1;
+      } if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1;
+      } if (a.title.toLowerCase() > b.title.toLowerCase()) {
+        return 1;
+      }
+      return 0;
     })
-  } return dataOrdenadaA;
-};
-
-//función de orden alfabético
-export const sortDataAlph = (data, condition) => {
-  let dataOrdenadaB = "";
-  if (condition === "az") {
-    dataOrdenadaB = data.sort((a, b) => {
+  } else if (condition === "az") {
+    dataOrdenada = data.sort((a, b) => {
       const firstParameter = a.title.toLowerCase();
       const secondParameter = b.title.toLowerCase();
       return (firstParameter < secondParameter) ? -1 : (firstParameter > secondParameter) ? 1 : 0;
     })
   } else {
-    dataOrdenadaB = data.sort((a, b) => {
+    dataOrdenada = data.sort((a, b) => {
       const firstParameter = a.title.toLowerCase();
       const secondParameter = b.title.toLowerCase();
       return (firstParameter > secondParameter) ? -1 : (firstParameter < secondParameter) ? 1 : 0;
     })
-  } return dataOrdenadaB;
+  } return dataOrdenada;
 };
-
-// export const sortData = (sortData, ghibliAnimations) => {
-//     if (sortData === "firstMovies") {
-//       ghibliAnimations.sort((a, b) => {
-//         let firstParameter = a.release_date;
-//         let secondParameter = b.release_date;
-//         return (firstParameter < secondParameter) ? -1 : (firstParameter > secondParameter) ? 1 : 0;
-//       })
-//     }
-//     if (sortData === "lastMovies") {
-//       ghibliAnimations.sort((a, b) => {
-//         let firstParameter = a.release_date;
-//         let secondParameter = b.release_date;
-//         return (firstParameter > secondParameter) ? -1 : (firstParameter < secondParameter) ? 1 : 0;
-//       })
-//     }
-//     return ghibliAnimations;
-//   };
-
-//   //se ve la página pero no corre
-//    export const sortData = (sortData, ghibliAnimations) => {
-//        if (sortData === "firstMovies") {
-//          ghibliAnimations.sort((a, b) => {
-//            if (a.release_date < b.release_date) {
-//              return 1;
-//            } if (a.release_date > b.release_date) {
-//              return -1;
-//            } if (a.title.toLowerCase() < b.title.toLowerCase()) {
-//              return 1;
-//            } if (a.title.toLowerCase() > b.title.toLowerCase()) {
-//              return -1;
-//            }
-//            return 0;
-//          })
-//        } else if (sortData === "lastMovies") {
-//          ghibliAnimations.sort((a, b) => {
-//            if (a.release_date > b.release_date) {
-//              return 1;
-//            } if (a.release_date < b.release_date) {
-//              return -1;
-//            } if (a.title.toLowerCase() > b.title.toLowerCase()) {
-//              return 1;
-//            } if (a.title.toLowerCase() < b.title.toLowerCase()) {
-//              return -1;
-//            }
-//            return 0;
-//          })
-//        }
-//      };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { sortDataAlph, sortDataRelease } from './data.js'; //impotando funciones
+import { sortData } from './data.js'; //impotando funciones
 import data from './data/ghibli/ghibli.js'; //importando data desde ghibli.js
 
 //llamando a la data desde ghibli.js
-const ghibliAnimations = data.films; 
+const ghibliAnimations = data.films;
 
 //función para mostrar las tarjetas de películas
 function showAnimations() {
@@ -22,20 +22,11 @@ function showAnimations() {
 showAnimations(ghibliAnimations);
 
 //función para mostrar el orden
-document.getElementById("orderRelease").addEventListener("change", showOrderRelease);
-document.getElementById("orderAlph").addEventListener("change", showOrderAlph);
+document.getElementById("order").addEventListener("change", showOrder);
 
-//orden por año de estreno
-function showOrderRelease() {
-  const orderOption = document.getElementById("orderRelease").value;
-  const orderResult = sortDataRelease(ghibliAnimations, orderOption);
-  showAnimations(orderResult);
-}
-
-//orden alfabético
-function showOrderAlph() {
-  const orderOption = document.getElementById("orderAlph").value;
-  const orderResult = sortDataAlph(ghibliAnimations, orderOption);
+function showOrder() {
+  const orderOption = document.getElementById("order").value;
+  const orderResult = sortData(ghibliAnimations, orderOption);
   showAnimations(orderResult);
 }
 
