ccd8f26fb877e22a686e92b6793fd91d648d1c50
Alejandra Castillo <castilloavilaa@gmail.com>
2023-01-03 13:20:04 -0500

    Mostrar y ocultar las páginas de cada película.

--- src/main.js
+++ src/main.js
@@ -9,12 +9,13 @@ function showAnimations() {
   let movies = "";
   ghibliAnimations.forEach((elements, i) => {
     movies +=
-      `<div class="caja">
+      `<div class="box">
       <img class="poster" src=${data.films[i].poster} />
       <h3>${data.films[i].title}</h3>
       <p>Director: ${data.films[i].director}</p>
       <p>Producer: ${data.films[i].producer}</p>
       <p>Release date: ${data.films[i].release_date}</p>
+      <button id="${data.films[i].id}" class="buttonSecondPage">See more</button>
       </div>`;
   });
   document.getElementById("animations").innerHTML = movies;
@@ -46,6 +47,20 @@ function filterByDirtector() {
   //alert(porDirector.title);
 }
 
+//función para filtrar por productores (funciona con el change y muestra en la consola)
+document.getElementById("producer").addEventListener("change", filterByProducer);
+function filterByProducer() {
+  const byProducer = ghibliAnimations.filter((pelicula) => {
+    if (pelicula.producer === document.getElementById("producer").value) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  console.log(byProducer);
+  //alert(porDirector.title);
+}
+
 //botón totoro
 document.getElementById("button-up").addEventListener("click", scrollUp);
 
@@ -55,3 +70,18 @@ function scrollUp() {
     window.scrollTo(0, 0);
   }
 }
+
+//mostrando y ocultando segunda página (por película)
+//mostrando 
+const buttonSecondPage = document.querySelectorAll(".buttonSecondPage");
+buttonSecondPage.forEach((el)=> el.addEventListener("click", () => {
+  document.getElementById("secondPage").style.display = "block";
+  document.getElementById("firstPage").style.display = "none";
+}));
+
+//ocultando
+document.getElementById("buttonGoBack").addEventListener("click", () => {
+  document.getElementById("secondPage").style.display = "none";
+  document.getElementById("firstPage").style.display = "block";
+  //window.location.reload(true);
+});
